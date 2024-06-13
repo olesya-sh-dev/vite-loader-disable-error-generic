fa13836023297396958022aebe6b57c2bb079dc8
Olesya Shalay <olesyakublickaya@yandex.ru>
2024-06-13 22:07:15 +0300

    error's catching

--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -1,16 +1,18 @@
+
 import { Dispatch } from 'redux'
 import { decksAPI, UpdateDeckParams } from './decks-api.ts'
 import { addDeckAC, deleteDeckAC, setDecksAC, updateDeckAC } from './decks-reducer.ts'
 import { setAppStatusAC } from '../../app/app-reducer.ts'
+import { isAxiosError } from 'axios'
 
 export const fetchDecksTC = () => async (dispatch: Dispatch) => {
-  dispatch(setAppStatusAC("loading"))
-  try{
+  dispatch(setAppStatusAC('loading'))
+  try {
     const res = await decksAPI.fetchDecks()
-      dispatch(setDecksAC(res.data.items))
-      dispatch(setAppStatusAC("succeeded"))
-  } catch (e){
-    dispatch(setAppStatusAC("failed"))
+    dispatch(setDecksAC(res.data.items))
+    dispatch(setAppStatusAC('succeeded'))
+  } catch (e) {
+    dispatch(setAppStatusAC('failed'))
   }
 }
 
@@ -26,8 +28,32 @@ export const deleteDeckTC = (id: string) => async (dispatch: Dispatch) => {
   })
 }
 
+//case-1: ошибка бэкэнда. Ошибку создает axios, в e.response.data помещает ответ сервера
+//case-2: network error - axios создаёт объект ошибки, сообщение можно взять из поля e.message
+//case-3: синхронные ошибки - создается нативная JS-ошибка, имеет поле e.message
+
 export const updateDeckTC = (params: UpdateDeckParams) => async (dispatch: Dispatch) => {
-  return decksAPI.updateDeck(params).then((res) => {
+  try {
+    //throw new Error('Error')
+    const res = await decksAPI.updateDeck(params)
     dispatch(updateDeckAC(res.data))
-  })
+  } catch (e: any) {
+    console.log(e)
+
+    let errorMessage: string
+    if (isAxiosError<ServerError>(e)) {
+      errorMessage = e.response ? e.response.data.errorMessages[0].message : e.message
+     
+    }
+   
+    else {
+      errorMessage = (e as Error).message
+    }
+
+    console.log(errorMessage)
+  }
+}
+
+type ServerError = {
+  errorMessages: Array<{ field: string; message: string }>
 }
